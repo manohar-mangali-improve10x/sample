16d6f20308f7d1c34aa0b28bbc153a67bc235b71
Manohar <manohar.mangali@improve10x.com>
2023-02-01 11:22:47 +0530

    added

---
+++ app/src/main/java/com/example/sample/BankAccountActivity.java
@@ -0,0 +1,14 @@
+package com.example.sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BankAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bankaccount);
+    }
+}
